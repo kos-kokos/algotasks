51bbb8cb91d9feb80d12a2d016f509c7568936b5
K.Devyatkin <k.devyatkin@tcsbank.ru>
2017-09-12 20:29:09 +0300

    first task

--- a-postfix/postfix.py
+++ a-postfix/postfix.py
@@ -1,3 +1,22 @@
 f = open('postfix.in', 'r')
-fileLine = f.readline(10000)
-print(fileLine)
+file_line = f.readline()
+f.close()
+stack = []
+for val in  file_line.split(sep=" "):
+    if val == "+" or val == "-" or val == "*":
+        val2 = stack.pop()
+        val1 = stack.pop()
+        if(val == "+"):
+            stack.append(val1 + val2)
+        elif val=="-":
+            stack.append(val1 - val2)
+        else:
+            stack.append(val1 * val2)
+    else:
+        stack.append(int(val))
+
+result_file = open("postfix.out", 'w')
+result_file.write(str(stack.pop()))
+result_file.flush()
+result_file.close()
+
