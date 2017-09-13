94c3ada33904058f53eac50d4ee9a34dc08ac16a
K.Devyatkin <k.devyatkin@tcsbank.ru>
2017-09-13 20:03:37 +0300

    first task

--- b-olimp/b-olimp.py
+++ b-olimp/b-olimp.py
@@ -1,8 +1,3 @@
-n = int(input("Input N"))
-points_str = input("Input points")
-points_splitted = points_str.split(" ")
-points = []
-
 def merge_sort(ar: list):
    if len(ar) == 1:
        return ar
@@ -21,20 +16,28 @@ def merge(ar1: list, ar2:list):
     j = 0
     result = []
     while i<len(ar1) or j<len(ar2):
-        val1 = ar1[i]
-        val2 = ar2[j]
-        #merging
-        if compareTuples(val1, val2):
-            result.append(val1)
+        if(i >= len(ar1)):
+            result.append(ar2[j])
+            j+=1
+        elif j>= len(ar2):
+            result.append(ar1[i])
             i+=1
         else:
-            result.append(val2)
-            j+=1
+            val1 = ar1[i]
+            val2 = ar2[j]
+
+            #merging
+            if compareTuples(val1, val2):
+                result.append(val1)
+                i+=1
+            else:
+                result.append(val2)
+                j+=1
     return result
 
 
 def compareTuples(t1,t2):
-    return t2[1] > t1[1]
+    return t2[1] < t1[1]
 
 def sort(ar: list):
     if ar[0][1]< ar[1][1]:
@@ -42,7 +45,13 @@ def sort(ar: list):
     else:
         return ar
 
+n = int(input("Input N"))
+points_str = input("Input points")
+points_splitted = points_str.split(" ")
+points = []
 
 for i in range(n):
-    points.append((i, int(points_splitted[i])))
-print(merge_sort(points))
+    points.append((i+1, int(points_splitted[i])))
+sort_result = merge_sort(points)
+result_str = ' '.join([str(x[0]) for x in sort_result])
+print(result_str)
